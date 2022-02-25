a879928ade0d280707c4496b8512addf4eb1a2bc
Andresito <ramirezandres1994@gmail.com>
2022-02-25 12:40:07 -0400

    add user controller add endpoint to list user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthCotroller;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthCotroller::class, 'login']);
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::group(['prefix' => 'user'], function () {
+       Route::get('',[UserController::class, 'list']);
+    });
 });
