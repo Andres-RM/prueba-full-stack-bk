fce543b9936d625bd448cd3a63f915217b9b1ec4
Andresito <ramirezandres1994@gmail.com>
2022-02-25 12:44:59 -0400

    add create user request

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|string|max:45',
+            'email' => 'required|string|max:195',
+            'name' => 'required|string|max:50',
+            'phone' => 'required|string|max:25',
+            'password' => 'required|string|min:6|max:20'
+        ];
+    }
+}
